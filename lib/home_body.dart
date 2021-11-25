254a73cf0441a9593142098aa068066bb6b6f163
rusnee awae <s6035512050@phuket.psu.ac.th>
2021-11-25 16:26:20 +0700

    project

--- lib/home_body.dart
+++ lib/home_body.dart
@@ -1,56 +1,106 @@
 //import 'dart:html';
-import 'package:authentification/category/category.dart';
-import 'package:authentification/custom_appBar.dart';
-import 'package:authentification/products/recent_products.dart';
-import 'package:authentification/search_section.dart';
-import 'package:authentification/slider/slider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+
 import 'package:flutter/material.dart';
 import 'package:authentification/personal.dart';
 import 'package:authentification/constants.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'HomePage.dart';
 
-class HomeBody extends StatefulWidget{
+class HomeBody extends StatefulWidget {
   @override
   _HomeBodyState createState() => _HomeBodyState();
 }
 
- class _HomeBodyState extends State<HomeBody>{
-   @override
-  Widget build(BuildContext context){
-     return Scaffold(
+class _HomeBodyState extends State<HomeBody> {
+  get kPrimaryColor => null;
 
-       body: SafeArea(
-         child: Padding(
-           padding: const EdgeInsets.all(8.0),
-           child: Column(
-             children: [
-               CustomAppBAr(),
-               SizedBox(
-                height: 20,
-               ),
-               SearchBAr(),
-               SizedBox(
-                 height: 20,
-               ),
-               ProductSlider(),
-               SizedBox(
-                 height: 20,
-               ),
-               Category(),
-               SizedBox(
-                 height: 20,
-               ),
-               Container(height:100,child: RecentProdects()),
-             ],
-           ),
-         ),
-       ),
-       drawer: Drawer(),
-     );
-   }
- }
+  @override
+  Widget build(BuildContext context) {
+    var row = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween, // ทำให้มันอยู่ตรงกลาง
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Builder(
+            builder: (context) => IconButton(
+              icon: Icon(Icons.dashboard),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => HomePage()), //กดlinkหน้า
+                );
+              },
+            ),
+          ),
+        ),
+        Column(
+          children: [
+            Text('Umar'),
+            Text(
+              'lonirgkf , ppfp',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+        CircleAvatar(
+          backgroundColor: kPrimaryColor,
+        ),
+      ],
+    );
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              row,
+              Column(
+                children: [CustomAppBAr()],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-
-
+  CustomAppBAr() {
+    return Row(
+      children: [
+        Expanded(
+            child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [BoxShadow(color: Colors.black38, blurRadius: 4)],
+          ),
+          child: TextField(
+              decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide.none,
+            ),
+            prefixIcon: Icon(Icons.search),
+          )),
+        )),
+        SizedBox(
+          width: 10,
+        ),
+        Container(
+            decoration: BoxDecoration(
+                color: kPrimaryColor,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(color: Colors.black38, blurRadius: 4)]),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.sort_by_alpha),
+            ))
+      ],
+    );
+  }
+}
